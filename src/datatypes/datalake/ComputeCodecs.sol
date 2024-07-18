746a5b334585b9fd943d715413ad6503c996c20c
Pia <gayeongparkk@gmail.com>
2024-07-18 17:44:35 +0200

    rm: redundant encode/decode

--- src/datatypes/datalake/ComputeCodecs.sol
+++ src/datatypes/datalake/ComputeCodecs.sol
@@ -39,40 +39,22 @@ enum Operator {
 /// @notice Codecs for ComputationalTask.
 /// @dev Represent a computational task with an aggregate function and context.
 library ComputationalTaskCodecs {
-    /// @dev Encodes a ComputationalTask.
-    /// @param task The ComputationalTask to encode.
-    function encode(ComputationalTask memory task) internal pure returns (bytes memory) {
-        return abi.encode(task.aggregateFnId, task.operatorId, task.valueToCompare);
-    }
-
-    /// @dev Encodes a ComputationalTask.
-    /// @param task The ComputationalTask to encode.
-    function encode_task(ComputationalTask memory task, bytes memory encodedDatalake)
-        internal
-        pure
-        returns (bytes memory)
-    {
-        return abi.encode(
-            TaskCode.Module, encodedDatalake, abi.encode(task.aggregateFnId, task.operatorId, task.valueToCompare)
-        );
-    }
-
     /// @dev Get the commitment of a ComputationalTask.
     /// @notice The commitment embeds the datalake commitment.
     /// @param task The ComputationalTask to commit.
     /// @param datalakeCommitment The commitment of the datalake.
-    function commit(ComputationalTask memory task, bytes32 datalakeCommitment) internal pure returns (bytes32) {
-        return keccak256(abi.encode(datalakeCommitment, task.aggregateFnId, task.operatorId, task.valueToCompare));
-    }
-
-    /// @dev Decodes a ComputationalTask.
-    /// @param data The encoded ComputationalTask.
-    function decode(bytes memory data) internal pure returns (ComputationalTask memory) {
-        (uint8 aggregateFnId, uint8 operator, uint256 valueToCompare) = abi.decode(data, (uint8, uint8, uint256));
-        return ComputationalTask({
-            aggregateFnId: AggregateFn(aggregateFnId),
-            operatorId: Operator(operator),
-            valueToCompare: valueToCompare
-        });
+    function commit(
+        ComputationalTask memory task,
+        bytes32 datalakeCommitment
+    ) internal pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    datalakeCommitment,
+                    task.aggregateFnId,
+                    task.operatorId,
+                    task.valueToCompare
+                )
+            );
     }
 }
--- src/datatypes/module/ModuleCodecs.sol
+++ src/datatypes/module/ModuleCodecs.sol
@@ -14,27 +14,9 @@ struct Module {
 /// @notice Codecs for Module.
 /// @dev Represent a computation perform by a module.
 library ModuleCodecs {
-    /// @dev Encodes a Module.
-    /// @param module The Module to encode.
-    function encode_task(
-        Module memory module
-    ) internal pure returns (bytes memory) {
-        return abi.encode(TaskCode.Module, module.classHash, module.inputs);
-    }
-
     /// @dev Get the commitment of a Module.
     /// @param module The Module to commit.
     function commit(Module memory module) internal pure returns (bytes32) {
         return keccak256(abi.encode(module.classHash, module.inputs));
-    }
-
-    /// @dev Decodes a Module.
-    /// @param data The encoded Module.
-    function decode(bytes memory data) internal pure returns (Module memory) {
-        (, bytes32 classHash, bytes32[] memory inputs) = abi.decode(
-            data,
-            (TaskCode, bytes32, bytes32[])
-        );
-        return Module(classHash, inputs);
     }
 }
