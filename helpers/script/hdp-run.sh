56c03d97a0750f234abaabab5dc274c9dd02171c
Pia <gayeongparkk@gmail.com>
2024-07-22 18:06:35 +0200

    chain id adjustment

--- helpers/script/hdp-run.sh
+++ helpers/script/hdp-run.sh
@@ -16,6 +16,6 @@ prepare_cairo_enviroment() {
 # Call the function to ensure the virtual environment is activated
 prepare_cairo_enviroment
 
-hdp encode -a -p helpers/target/bs_cached_input.json -o helpers/target/bs_cached_output.json -c helpers/target/bs_hdp_pie.zip slr none.10000000 -b 5858987 5858997 header.excess_blob_gas 2
-hdp encode -a -p helpers/target/tx_cached_input.json -o helpers/target/tx_cached_output.json -c helpers/target/tx_hdp_pie.zip slr none.50 -t 5605816 tx_receipt.success 12 53 1 0,0,1,1
-hdp run-module 0x4F21E5,0x4F21E8,0x13cb6ae34a13a0977f4d7101ebc24b87bb23f0d5 --class-hash 0x02aacf92216d1ae71fbdaf3f41865c08f32317b37be18d8c136d442e94cdd823 -p helpers/target/md_cached_input.json -o helpers/target/md_cached_output.json -c helpers/target/md_hdp_pie.zip
+hdp run -r helpers/target/bs_request.json -p helpers/target/bs_cached_input.json -o helpers/target/bs_cached_output.json -c helpers/target/bs_hdp_pie.zip
+hdp run -r helpers/target/tx_request.json -p helpers/target/tx_cached_input.json -o helpers/target/tx_cached_output.json -c helpers/target/tx_hdp_pie.zip
+# hdp run-module 0x4F21E5,0x4F21E8,0x13cb6ae34a13a0977f4d7101ebc24b87bb23f0d5 --class-hash 0x00ababb33ae5911fd14e6b9f2853b6271f553b9ec7835298134f4bb020100971 -p helpers/target/md_cached_input.json -o helpers/target/md_cached_output.json -c helpers/target/md_hdp_pie.zip
